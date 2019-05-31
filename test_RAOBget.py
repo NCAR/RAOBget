7e7e0b33abe55278e2671227766f2c899e2aa1ec
janineA <janine@ucar.edu>
2019-05-31 11:34:51 -0600

    Set defaults for test case and explicit check on URL

--- test_RAOBget.py
+++ test_RAOBget.py
@@ -1,6 +1,18 @@
+#!/usr/local/bin/python3
 import unittest
 
 from RAOBget import RAOBget
+
+
+# Set default values for testing that match test data.
+class options():
+    region = 'North America'
+    raobtype = 'TEXT:LIST'
+    year = "2019"
+    month = "05"
+    begin = "2812"
+    end = "2812"
+    stnm = "72672"
 
 
 class TestRAOBstation_list(unittest.TestCase):
@@ -9,7 +21,25 @@ class TestRAOBstation_list(unittest.TestCase):
 
         self.raob = RAOBget()
 
+        # Set requested station to default values
+        self.option=options()
+
+        self.raob.set_type(self.option)
+        self.raob.set_region(self.option)
+        self.raob.set_year(self.option)
+        self.raob.set_month(self.option)
+        self.raob.set_begin(self.option)
+        self.raob.set_end(self.option)
+        self.raob.set_stnm(self.option)
+
     def test_TEXT_LIST(self):
+
+        ctrlurl = "http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&" + \
+                  "TYPE=TEXT%3ALIST&YEAR=2019&MONTH=05&FROM=2812&TO=2812&" + \
+                  "STNM=72672"
+
+        url = self.raob.get_url_textlist()
+        self.assertEqual(url, ctrlurl)
 
         outfile = self.raob.retrieve_textlist()
 
