f6881eb4eba01a3925d64d88d1185a0da0510cec
janineA <janine@ucar.edu>
2019-06-19 16:28:00 -0600

    Fix bug -accidentally double added root dir to path

--- src/lib/rsl.py
+++ src/lib/rsl.py
@@ -6,7 +6,6 @@
 #
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2019
 ###############################################################################
-from lib.raobroot import getrootdir
 
 
 class RSL():
@@ -14,7 +13,7 @@ class RSL():
     def read_rsl(self, args):  # read RAOB Station List (RSL) file
         """ Read a optional provided list of raob stations to request data for
         """
-        rsl = open(getrootdir() + "/" + args.rsl)
+        rsl = open(args.rsl)
         stnlist = rsl.readlines()
         rsl.close()
         return([line.rstrip() for line in stnlist])
