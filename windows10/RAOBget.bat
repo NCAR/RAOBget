a02439bca5499848ee31689b0b633a7549d5863f
unknown <lroot@eol-atom.eol.ucar.edu>
2021-08-03 14:30:27 -0600

    Use python on path rather than hardcoding path to a specific checkout

--- windows10/RAOBget.bat
+++ windows10/RAOBget.bat
@@ -1,4 +1,3 @@
 @echo on
-call C:\Users\lroot\Miniconda3\Scripts\activate.bat
-C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\RAOBget\src\RAOBget.py --gui
+python.exe C:\Users\lroot\RAOBget\src\RAOBget.py --gui
 pause
